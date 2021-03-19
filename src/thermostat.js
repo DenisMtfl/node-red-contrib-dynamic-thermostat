3781cc87700fdea047fddb74adf0defd27ce740a
Denis Manteufel <denis.manteufel@gmail.com>
2021-03-19 21:18:45 +0100

    lint fix

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,67 +1,59 @@
-//var targetValue = RED.util.evaluateNodeProperty(node.config.target, node.config.targetType, node, msg);
+module.exports = function (RED) {
+  function Thermostat (config) {
+    RED.nodes.createNode(this, config)
 
-module.exports = function(RED) {
-    function Thermostat(config) {
-        RED.nodes.createNode(this, config);    
-                                              
-        var node = this;
-        var nodeContext = this.context();
-        node.config = config;
-        node.context = nodeContext;
-        
-        node.on('input', function(msg) 
-        {    
-            var statusColor = "red";
-            var result = null;
+    const node = this
+    const nodeContext = this.context()
+    node.config = config
+    node.context = nodeContext
 
-            node.target = null;
-            node.current = null;
-            node.hysteresis = null;                
+    node.on('input', function (msg) {
+      let statusColor = 'red'
+      let result = null
 
-            if(msg.topic == 'target')
-            {                
-                node.context.set('target', msg.payload);
-            } else
-            if(msg.topic == 'current')
-            {
-                node.context.set('current', msg.payload);                
-            } else
-            if(msg.topic == 'hysteresis')
-            {
-                node.context.set('hysteresis', msg.payload);                
-            }
+      node.target = null
+      node.current = null
+      node.hysteresis = null
 
-            node.target = node.context.get('target');
-            node.current = node.context.get('current')
-            node.hysteresis = node.context.get('hysteresis')                    
+      if (msg.topic === 'target') {
+        node.context.set('target', msg.payload)
+      } else
+      if (msg.topic === 'current') {
+        node.context.set('current', msg.payload)
+      } else
+      if (msg.topic === 'hysteresis') {
+        node.context.set('hysteresis', msg.payload)
+      }
 
-            if(node.current != undefined && node.target != undefined && node.hysteresis != undefined)
-            {
-                statusColor = "green";                                                
-                result = Calc(node.current, node.target, node.hysteresis);                                        
-            }
+      node.target = node.context.get('target')
+      node.current = node.context.get('current')
+      node.hysteresis = node.context.get('hysteresis')
 
-            this.status({fill: statusColor,shape:"dot", text: `Current: ${node.current}  Target: ${node.target} Hysteresis: ${node.hysteresis}`});     
-            var msg = { payload: { onoff: result, current: node.current, target: node.target, hysteresis: node.hysteresis } };
-            node.send(msg);
-        });            
+      if (node.current !== undefined && node.target !== undefined && node.hysteresis !== undefined) {
+        statusColor = 'green'
+        result = Calc(node.current, node.target, node.hysteresis)
+      }
+
+      this.status({ fill: statusColor, shape: 'dot', text: `Current: ${node.current}  Target: ${node.target} Hysteresis: ${node.hysteresis}` })
+      msg = { payload: { onoff: result, current: node.current, target: node.target, hysteresis: node.hysteresis } }
+      node.send(msg)
+    })
+  }
+
+  function Calc (current, target, hysteresis) {
+    // Heater on
+    if (current < target - hysteresis) {
+      return true
     }
 
-    function Calc(current, target, hysteresis)
-    {
-        // Heater on
-        if (current < target - hysteresis) {
-            return true;
-        }
-
-        // Heater off
-        if (current > target + hysteresis) {
-            return false;
-        }  
-        
-        //Nothing to control
-        return null;
+    // Heater off
+    if (current > target + hysteresis) {
+      return false
     }
 
-    RED.nodes.registerType("thermostat", Thermostat);   
+    // Nothing to control
+    return null
+  }
+
+  RED.nodes.registerType('thermostat', Thermostat)
 }
