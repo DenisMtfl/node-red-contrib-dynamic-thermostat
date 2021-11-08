5917732ea4406a4b129b8a85a6a614bc833548bc
Denis Mtfl <1503277+DenisMtfl@users.noreply.github.com>
2021-11-08 14:50:56 +0100

    fix

--- src/thermostat.js
+++ src/thermostat.js
@@ -33,10 +33,14 @@ module.exports = function (RED) {
 
       if (node.current !== undefined && node.target !== undefined && node.hysteresis !== undefined) {
         statusColor = 'green'
-        if(node.switch === false)                
+        if(node.switch === false) 
+        {               
           result = Calc(node.current, node.target, node.hysteresis)
+        }
         else
+        {
           return true
+        }
       }
 
       this.status({ fill: statusColor, shape: 'dot', text: `Current: ${node.current}  Target: ${node.target} Hysteresis: ${node.hysteresis} Switch: ${node.switch}` })
